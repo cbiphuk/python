139548160f32ea27363701183fad50ab7d817bd1
Vitalii Lirnyk <vlirnyk@agorafy.com>
2016-10-24 14:54:06 -0400

    few functions for monitoring script are added

--- aws/monitoring_scripts/service_monitoring.py
+++ aws/monitoring_scripts/service_monitoring.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python2.7
 
+
 import os.path
 import httplib
 import datetime
 
 #AWS namespace
 namespace = ''
+#TODO: add enable_monitoring feature
 enable_monitoring = True
 
 #nginx config
@@ -24,74 +26,102 @@ mysql_service_name = 'MysqlService'
 mysql_system_name = 'mysql'
 
 #webservice checker
-webservice_name = 'NewsroomWebService'
+webservice_name = 'WebService'
 webservice_url = 'example.com'
 
 def WebserviceChecker(service_name, url):
   """ Checks webservice for current URL"""
 
-  date = '{:%Y-%m-%dT%H:%M:%S}'.format(datetime.datetime.now())
+  #date = '{:%Y-%m-%dT%H:%M:%S}'.format(datetime.datetime.now())
 
   conn = httplib.HTTPConnection(webservice_url, 80)
   try:
     conn.request("HEAD", "/")
   except:
-    print date + ': '+ webservice_name + ' is not responding'
+    log_message = 'is not responding'
+    PushLog(service_name, log_message)
+    #print date + ': '+ webservice_name + ' is not responding'
     return (service_name, False)
   else:
     r1 = conn.getresponse()
-    print date + ': ' + service_name + ' is alive ' + str(r1.status), r1.reason
+    log_message = 'is alive ' + str(r1.status) + ' ' + r1.reason
+    PushLog(service_name, log_message)
+    #print date + ': ' + service_name + ' is alive ' + str(r1.status), r1.reason
     return (service_name, True)
 
 def IsAlive(pid_file, service_name, system_name):
   """ This function checks of process status and tries to restart it if the process does not launched"""
 
-  date = '{:%Y-%m-%dT%H:%M:%S}'.format(datetime.datetime.now())
+  #date = '{:%Y-%m-%dT%H:%M:%S}'.format(datetime.datetime.now())
   if os.path.isfile(pid_file):
-    print date + ': ' + service_name + " pid is over there"
+    log_message = 'pis is over there'
+    PushLog(service_name, log_message)
+    #print date + ': ' + service_name + " pid is over there"
     the_file = open(pid_file, 'r')
     pid = the_file.readline()
     try:
       os.kill(int(pid), False)
-      print date + ': ' + service_name + " process is running"
+      log_message = 'process is running'
+      PushLog(service_name, log_message)
+      #print date + ': ' + service_name + " process is running"
       return (service_name, True)
     except:
-      print date + ': ' + service_name + " pid file exist but process is dead"
+      log_message = 'pid file exist but process is dead'
+      PushLog(service_name, log_message)
+      #print date + ': ' + service_name + " pid file exist but process is dead"
       serviceRestart(system_name)
       return (service_name, False)
   else:
-    print date + ': ' + service_name + " is DEAD!"
+    log_message = 'is DEAD!'
+    PushLog(service_name, log_message)
+    #print date + ': ' + service_name + " is DEAD!"
     serviceRestart(system_name)
     return (service_name, False)
 
 def AWSSendStatus(service):
   """Sends status of current process to AWS Cloudwatch.
-  TODO: use aws api instead of aws-cli
   """
+  #TODO: use aws api instead of aws-cli
+  
   status = service[1]
   service_name = service[0]
-  date = '{:%Y-%m-%dT%H:%M:%S}'.format(datetime.datetime.now())
+  #date = '{:%Y-%m-%dT%H:%M:%S}'.format(datetime.datetime.now())
 
   if status:
     value = 1
     command = '/usr/local/bin/aws cloudwatch put-metric-data --metric-name ' \
-    + service_name + ' --namespace ' + namespace + ' --value ' + str(value) + ' --timestamp '+ date
+    + service_name + ' --namespace ' + namespace + ' --value ' + str(value) + \
+    ' --timestamp '+ GetDate()
     #print command
   else:
     value = 0
     command = '/usr/local/bin/aws cloudwatch put-metric-data --metric-name ' \
-    + service_name + ' --namespace ' + namespace + ' --value ' + str(value) + ' --timestamp '+ date
+    + service_name + ' --namespace ' + namespace + ' --value ' + str(value) + \
+    ' --timestamp '+ GetDate()
     #print command
   os.system(command)
 
 def serviceRestart(service_name):
   """Restarts current service"""
 
-  date = '{:%Y-%m-%dT%H:%M:%S}'.format(datetime.datetime.now())
+  #date = '{:%Y-%m-%dT%H:%M:%S}'.format(datetime.datetime.now())
   command = 'sudo service ' + service_name + ' restart'
-  print date + ': ' + service_name + ' restarting'
+  log_message = 'restarting'
+  PushLog(service_name, log_message)
+  #print GetDate() + ': ' + service_name + ' restarting'
   os.system(command)
 
+def PushLog(service_name, message):
+  #date = '{:%Y-%m-%dT%H:%M:%S}'.format(datetime.datetime.now())
+  print GetDate() + ': ' + service_name + ' ' + message
+
+def GetDate():
+  date = '{:%Y-%m-%dT%H:%M:%S}'.format(datetime.datetime.now())
+  return date
+
+
+
+#MAIN
 if __name__ == '__main__':
   AWSSendStatus(IsAlive(nginx_pid, nginx_name, nginx_system_name))
   AWSSendStatus(IsAlive(php_fpm_pid, php_fpm_name, php_fpm_system_name))
